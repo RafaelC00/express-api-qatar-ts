4a9fa9abbf39f7875781f916de4ee7a252d596cd
RafaelC00 <rafacaicedo@hotmail.com>
2022-10-09 09:45:45 -0300

    port display

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.listen(app.get('port') , () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port '+app.get('port'));
 });
 
 app.get('/', (req, res) => {
